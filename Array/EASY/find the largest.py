6685bbf05da81ed1513f2d0d93fbbf1728eccbad
YogeshwaranE <yogeshwarane2002@outlook.com>
2024-08-07 14:36:53 +0530

    largest element in array

---
+++ Array/EASY/find the largest.py
@@ -0,0 +1,13 @@
+def largest(arr):
+    arr.sort()
+    return arr[-1]
+
+arr=[1,20,13,24,5]
+#print(largest(arr))
+
+def largest(arr):
+    return max(arr)
+arr=[1,20,13,2,5]
+print(largest(arr))
+
+
