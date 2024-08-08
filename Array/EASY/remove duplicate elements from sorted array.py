d8192dd63dd380a0af99604db47668392b8570b7
YogeshwaranE <yogeshwarane2002@outlook.com>
2024-08-08 14:59:34 +0530

    remove duplicate elements from list

---
+++ Array/EASY/remove duplicate elements from sorted array.py
@@ -0,0 +1,16 @@
+def removeduplicate(arr):
+
+    i=0
+    for j in range(1, len(arr)):
+        if arr[i]!= arr[j]:
+            i+=1
+            arr[i]=arr[j]
+    return arr[:i+1]
+
+arr = [1, 1, 2, 2, 2, 3, 3]
+print(removeduplicate(arr))
+
+
+
+
+
